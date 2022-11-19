efc32ec0eaef0c6d2bc53c83c8990572e5b64117
HaraT0323 <alra9si916323@gmail.com>
2022-11-19 21:49:57 +0900

    アクセスログの設定

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/api/hello", async (req, res) => {
   res.json({
