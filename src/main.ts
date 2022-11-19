6996af97c36d9ebac40dad53b0be633e7651ee30
HaraT0323 <alra9si916323@gmail.com>
2022-11-19 21:58:26 +0900

    エラーハンドリングの実装

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import "express-async-errors";
 
 const PORT = 3000;
 
@@ -13,6 +14,24 @@ app.get("/api/hello", async (req, res) => {
   });
 });
 
+app.get("/api/error", async (req, res) => {
+  throw new Error("Error endpoint");
+});
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`server started: http://localhost:${PORT}`);
 });
+
+function errorHandler(
+  err: any,
+  _req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction
+) {
+  console.error("Unexpected error occurred", err);
+  res.status(500).send({
+    message: "Unexpected error occurred",
+  });
+}
