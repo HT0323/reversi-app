a1f3b82d11e5773a1f366b6b9725095d0b5c82e4
HaraT0323 <alra9si916323@gmail.com>
2022-11-20 21:06:02 +0900

    リクエスト、レスポンスボディーの型定義

--- src/application/turnService.ts
+++ src/application/turnService.ts
@@ -10,8 +10,35 @@ const turnGateway = new TurnGateway();
 const moveGateway = new MoveGateway();
 const squareGateway = new SquareGateway();
 
+class FindLatestGameTurnByTurnCountOutput {
+  constructor(
+    private _turnCount: number,
+    private _board: number[][],
+    private _nextDisc: number | undefined,
+    private _winnerDisc: number | undefined
+  ) {}
+
+  get turnCount() {
+    return this._turnCount;
+  }
+
+  get board() {
+    return this._board;
+  }
+
+  get nextDisc() {
+    return this._nextDisc;
+  }
+
+  get winnerDisc() {
+    return this._winnerDisc;
+  }
+}
+
 export class TurnService {
-  async findLatestGameTurnByTurnCount(turnCount: number) {
+  async findLatestGameTurnByTurnCount(
+    turnCount: number
+  ): Promise<FindLatestGameTurnByTurnCountOutput> {
     const conn = await connectMySql();
 
     try {
@@ -40,12 +67,12 @@ export class TurnService {
         board[s.y][s.x] = s.disc;
       });
 
-      return {
+      return new FindLatestGameTurnByTurnCountOutput(
         turnCount,
         board,
-        nextDisc: turnRecord.nextDisc,
-        winnerDisc: null,
-      };
+        turnRecord.nextDisc,
+        undefined
+      );
     } finally {
       await conn.end();
     }
--- src/presentation/turnRouter.ts
+++ src/presentation/turnRouter.ts
@@ -1,34 +1,51 @@
 import express from "express";
-import { DARK, LIGHT } from "../application/constants";
 import { TurnService } from "../application/turnService";
-import { connectMySql } from "../dataaccess/connection";
-import { GameGeteway } from "../dataaccess/gameGeteway";
-import { MoveGateway } from "../dataaccess/moveGateway";
-import { SquareGateway } from "../dataaccess/squareGateway";
-import { TurnGateway } from "../dataaccess/turnGatewat";
 
 export const turnRouter = express.Router();
 const turnService = new TurnService();
 
-const gameGeteway = new GameGeteway();
-const turnGateway = new TurnGateway();
-const moveGateway = new MoveGateway();
-const squareGateway = new SquareGateway();
+interface TurnGetResponseBody {
+  turnCount: number;
+  board: number[][];
+  nextDisc: number | null;
+  winnerDisc: number | null;
+}
 
-turnRouter.get("/api/games/latest/turns/:turnCount", async (req, res) => {
-  const turnCount = parseInt(req.params.turnCount);
+turnRouter.get(
+  "/api/games/latest/turns/:turnCount",
+  async (req, res: express.Response<TurnGetResponseBody>) => {
+    const turnCount = parseInt(req.params.turnCount);
 
-  const output = await turnService.findLatestGameTurnByTurnCount(turnCount);
-  res.json(output);
-});
+    const output = await turnService.findLatestGameTurnByTurnCount(turnCount);
 
-turnRouter.post("/api/games/latest/turns", async (req, res) => {
-  const turnCount = parseInt(req.body.turnCount);
-  const disc = parseInt(req.body.move.disc);
-  const x = parseInt(req.body.move.x);
-  const y = parseInt(req.body.move.y);
+    const ResponseBody = {
+      turnCount: output.turnCount,
+      board: output.board,
+      nextDisc: output.nextDisc ?? null,
+      winnerDisc: output.winnerDisc ?? null,
+    };
+    res.json(ResponseBody);
+  }
+);
 
-  await turnService.registerTurn(turnCount, disc, x, y);
+interface TurnPostRequestBody {
+  turnCount: number;
+  move: {
+    disc: number;
+    x: number;
+    y: number;
+  };
+}
+turnRouter.post(
+  "/api/games/latest/turns",
+  async (req: express.Request<{}, {}, TurnPostRequestBody>, res) => {
+    const turnCount = req.body.turnCount;
+    const disc = req.body.move.disc;
+    const x = req.body.move.x;
+    const y = req.body.move.y;
 
-  res.status(201).end();
-});
+    await turnService.registerTurn(turnCount, disc, x, y);
+
+    res.status(201).end();
+  }
+);
